97d11ae72b87013bc11f1952e1086ce146b3c1a4
salaboy <salaboy@gmail.com>
2018-12-07 11:37:24 +0000

    adding istio tests

--- src/main/java/org/s1p/demo/gateway/GatewayApplication.java
+++ src/main/java/org/s1p/demo/gateway/GatewayApplication.java
@@ -1,16 +1,43 @@
 package org.s1p.demo.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GatewayApplication {
+@EnableScheduling
+public class GatewayApplication implements CommandLineRunner {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,
                 args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(">>>> Services: " + System.currentTimeMillis());
+        for (String s : discoveryClient.getServices()) {
+            System.out.println("\t >>>> Service: " + s);
+        }
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void printServices(){
+        System.out.println(">>>> Services: " + System.currentTimeMillis());
+        for (String s : discoveryClient.getServices()) {
+            System.out.println("\t >>>> Service: " + s);
+        }
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
     }
 
 }
